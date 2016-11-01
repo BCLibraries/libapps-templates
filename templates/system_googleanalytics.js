fdfe8902fa62d2c2c2de0c76fe708edd3b22d86e
Jesse Martinez <mjesse@gmail.com>
2016-11-01 16:01:43 -0400

    adding GA event tracking code

---
+++ templates/system_googleanalytics.js
@@ -0,0 +1,36 @@
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-XXXXXXX-XX', 'auto');
+ga('require', 'linkid');
+ga('send', 'pageview');
+
+$(function () {
+    $('#megamenu ul.nav li.dropdown').mouseenter(function() {
+        var myText = $(this).find("a.menu-option").text();
+        ga('send', 'event', 'megamenu', 'hover', myText.trim());
+    });
+   $("ul.item-desktop li a").mouseenter(function(){
+        var myPane = $(this).html();
+        ga('send', 'event', 'librarypanes', 'hover', myPane);
+    });
+    $(".sb-icon-search").click(function(){
+        ga('send', 'event', 'lib-searchbutton', 'click');
+    });
+    $("input#searchsubmit").click(function(){
+        ga('send', 'event', 'bc-searchbutton', 'click');
+    });
+    $(".breadcrumb").click(function(){
+        var page_title = $("#guidetitle h1").text();
+        ga('send', 'event', 'breadcrumb', 'click', page_title);
+    });
+    /* get page views originating from Canvas */
+    if ((window.location.pathname).indexOf('widget_')>-1){
+        var current_title = $(document).attr('title');
+        ga('send', 'event', 'canvas', 'view', current_title);
+    }
+});
+</script>
